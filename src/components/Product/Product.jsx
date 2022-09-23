7ba9523e53948b0b7af99e8fd64a0c5e05bfa164
isha <vshlkmr81@gmail.com>
2022-09-23 22:49:09 +0530

    new project adding

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,7 +3,9 @@ import '../Product/Product.css'
 
 const Product = ({img ,pic,title}) => {
   return (
+    
     <div className="p">
+      <div className='ti'><h4>{title}</h4></div>
     <div className="p-browser">
       <div className="p-circle"></div>
       <div className="p-circle"></div>
@@ -12,7 +14,7 @@ const Product = ({img ,pic,title}) => {
       <a href={img} target="_blank" rel="noreferrer">
       <img src={pic} alt="" className="p-img" />
     </a>
-    <div className='ti'><h4>{title}</h4></div>
+    
       
   </div>
   )
--- src/components/ProductList/Productlist.jsx
+++ src/components/ProductList/Productlist.jsx
@@ -4,7 +4,10 @@ import './Productlist.css'
 import moon from '../../img/secondpro.png'
 import rpic from '../../img/prosec.png'
 import gitlink from '../../img/dj.png'
-import come from '../../img/comingsoon.png'
+// import come from '../../img/comingsoon.png'
+import chat from '../../img/peoplechat.png'
+import mshop from '../../img/mobileshop.png'
+import mern from '../../img/mernpic.png'
 
 const Productlist = () => {
   return (
@@ -18,12 +21,13 @@ const Productlist = () => {
       </p>
     </div>
     <div className="pl-list">
-      <Product title="React Project"  pic={rpic} img="https://myvishalkumarcv.herokuapp.com/"/ >
+      <Product title="React Project"  pic={rpic} img="https://vishak.netlify.app/"/ >
       <Product title="Django Project"  pic={gitlink} img="https://github.com/v-isha/Monkeykeen"/ >
       <Product title="Python Project"  pic={moon} img="https://myvishalkumarcv.herokuapp.com/"/ >
-      <Product title="MERN Stack" pic={come}/>
-      <Product title="GYM Stack" pic={come}/>
-      <Product title="MonkeyFood Stack" pic={come}/>
+      <Product title="MobileShop" pic={mshop} img="https://vmobile.netlify.app/"/>
+      <Product title="MERN Stack" pic={mern} img="https://github.com/stars/v-isha/lists/ecommerce-mernstack"/>
+      <Product title="Chat APP" pic={chat} img="https://peoplechat.netlify.app/"/>
+      
       
             
       
